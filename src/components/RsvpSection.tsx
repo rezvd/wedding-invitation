c804e68824531b52b4cd7e42daab9dcef0b82153
Darya Rezvanova <daria.rezv@gmail.com>
2026-05-10 04:57:04 +0600

    feat: make confirmation section green

--- src/components/RsvpSection.tsx
+++ src/components/RsvpSection.tsx
@@ -2,7 +2,7 @@ import SectionCard from './SectionCard'
 
 function RsvpSection() {
   return (
-    <SectionCard title="Подтвердите ваше присутствие">
+    <SectionCard title="Подтвердите ваше присутствие" className="rsvp-card">
       <p className="body-copy centered">
         Нам будет очень приятно видеть вас среди гостей нашего праздника.
         Пожалуйста, сообщите о своём решении до{" "}
@@ -12,7 +12,7 @@ function RsvpSection() {
         Для подтверждения участия и получения актуальной информации,
         пожалуйста, присоединитесь к чату гостей во ВКонтакте по ссылке ниже.
       </p>
-      <a className="btn" href="#" aria-label="Подтвердить и вступить в чат">
+      <a className="btn rsvp-btn" href="#" aria-label="Подтвердить и вступить в чат">
         Подтвердить и вступить в чат
       </a>
     </SectionCard>
